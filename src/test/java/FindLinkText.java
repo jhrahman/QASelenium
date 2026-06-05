21596bbf03313d28fe70a299acf8d06ac2586d7f
Jahidur Rahman <jahidur011@gmail.com>
2026-06-05 17:31:09 +0600

    findlinktexts

--- src/test/java/FindLinkText.java
+++ src/test/java/FindLinkText.java
@@ -1,4 +1,15 @@
 package PACKAGE_NAME;
 
 public class FindLinkText {
+    WebDriver driver;
+    @Test
+    public void test3(){
+        driver = new EdgeDriver();
+        driver.get("https://the-internet.herokuapp.com/");
+        driver.findElement(By.linkText("Form Authentication")).click();
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
+        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.className("radius")).click();
+        driver.quit();
+    }
 }
