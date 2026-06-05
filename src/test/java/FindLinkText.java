91f5d34324cf805a2bf8aad87ab31f1a43980b3f
Jahidur Rahman <jahidur011@gmail.com>
2026-06-05 20:05:50 +0600

    find link text

--- src/test/java/FindLinkText.java
+++ src/test/java/FindLinkText.java
@@ -8,8 +8,8 @@ public class FindLinkText {
     @Test
     public void test3(){
         driver = new EdgeDriver();
-        driver.get("https://the-internet.herokuapp.com/");
-        driver.findElement(By.linkText("Form Authentication")).click();
+        driver.get("https://the-internet.herokuapp.com/"); //navigate the site
+        driver.findElement(By.linkText("Form Authentication")).click(); //find element by text link. Note that the text must be a link text
         driver.findElement(By.id("username")).sendKeys("tomsmith");
         driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
         driver.findElement(By.className("radius")).click();
