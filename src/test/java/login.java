93d7eb57522799d7742c648078a3f631090b496c
Jahidur Rahman <jahidur011@gmail.com>
2026-06-05 17:37:31 +0600

    find elements with id

---
+++ src/test/java/login.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+public class login {
+    WebDriver driver;
+    @Test
+    public void test1(){
+        driver = new EdgeDriver();
+        driver.get("https://the-internet.herokuapp.com/login");
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
+        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.ById.className("radius")).click();
+    }
+}
