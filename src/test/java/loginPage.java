4f3055a1222ee025e92bbbb26efef8510f15128c
Jahidur Rahman <jahidur011@gmail.com>
2026-06-05 17:39:43 +0600

    find elements by name

--- src/test/java/loginPage.java
+++ src/test/java/loginPage.java
@@ -9,8 +9,11 @@ public class loginPage {
     public void test2(){
         driver = new EdgeDriver();
         driver.get("https://the-internet.herokuapp.com/login");
-        driver.findElement(By.id("username")).sendKeys("tomsmith");
-        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.name("username")).sendKeys("tomsmith");
+        driver.findElement(By.name("password")).sendKeys("SuperSecretPassword!");
+
+//        driver.findElement(By.id("username")).sendKeys("tomsmith");
+//        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
         driver.findElement(By.className("radius")).click();
         driver.quit();
     }
