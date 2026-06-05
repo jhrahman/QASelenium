451e76f3c3b7b5aa3002851a4476a4434c7f352b
Jahidur Rahman <jahidur011@gmail.com>
2026-06-05 19:53:18 +0600

    partial link text

--- src/test/java/PartialLinkText.java
+++ src/test/java/PartialLinkText.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.Wait;
+import org.testng.annotations.Test;
 
 public class PartialLinkText {
+    WebDriver driver;
+    @Test
+    public void test4() throws InterruptedException{
+        driver = new EdgeDriver();
+        driver.get("https://the-internet.herokuapp.com/");
+        driver.findElement(By.linkText("Form Authentication")).click();
+        Thread.sleep(3000);
+        driver.navigate().back();
+        driver.findElement(By.partialLinkText("JavaScript onload")).click();
+        Thread.sleep(3000);
+        driver.quit();
+
+    }
 }
