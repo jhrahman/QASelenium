df2bf57b8f77df9e9e26f3c7ec654609fa7ad50a
Jahidur Rahman <jahidur011@gmail.com>
2026-06-05 17:02:31 +0600

    Add loginPage.java only

---
+++ src/test/java/loginPage.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+public class loginPage {
+    WebDriver driver;
+    @Test
+    public void test2(){
+        driver = new EdgeDriver();
+        driver.get("https://the-internet.herokuapp.com/login");
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
+        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.className("radius")).click();
+        driver.quit();
+    }
+}
